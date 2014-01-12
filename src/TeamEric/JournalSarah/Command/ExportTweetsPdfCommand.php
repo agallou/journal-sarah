de50fc3501f82cca2610059e9ebfbeccd2396b34
Adrien Gallou <adriengallou@gmail.com>
2014-01-12 12:52:06 +0100

    ajout une commande appellant pdflatex

--- src/TeamEric/JournalSarah/Command/ExportTweetsPdfCommand.php
+++ src/TeamEric/JournalSarah/Command/ExportTweetsPdfCommand.php
@@ -8,29 +8,15 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class ExportTweetsPdfCommand extends Command
 {
-    /**
-     * @var TwitterClient
-     */
-    private $client;
-
-    /**
-     * @param string        $name
-     * @param TwitterClient $client
-     */
-    public function __construct($name = null, TwitterClient $client)
-    {
-        $this->client = $client;
-        parent::__construct($name);
-    }
-
     protected function configure()
     {
         $this
             ->setName('export:pdf')
-            ->setDescription("Creer un fichier Latex d'après les tweets")
+            ->setDescription("Creer un fichier PDF d'après les tweets")
             ->addArgument(
                 'filename',
                 InputArgument::REQUIRED
@@ -47,55 +33,31 @@ class ExportTweetsPdfCommand extends Command
     {
         $filename = $input->getArgument('filename');
 
-        $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../../../../views/');
-        $twig = new \Twig_Environment($loader);
+        $command = $this->getApplication()->find('export:latex');
 
-        $filter = new \Twig_SimpleFilter('latex_escaping', function ($string) {
-          return str_replace(array('#', '@', '_'), array('\#', '\verb+@+', "\_"), $string);
-        }, array('is_safe' => array('html')));
-        $twig->addFilter($filter);
+        $rootDir = __DIR__ . '/../../../../';
+        $tmpDir = $rootDir . '/tmp';
 
-        $tweets = array();
-        foreach ($this->getTweetIdsToExport() as $id) {
-          $twitt = $this->prepareTweet($id);
-          $tweets[$twitt['timestamp']] = $twitt;
-        }
-        ksort($tweets);
-        file_put_contents($filename, $twig->render('output.latex.twig', array(
-          'tweets' => $tweets,
-          'with_background' => $input->getOption('with-background'),
-        )));
+        $arguments = array(
+          'command'  => 'export:latex',
+          'filename' => $tmpDir . DIRECTORY_SEPARATOR . '/output.latex',
+          '--with-background' => $input->getOption('with-background'),
+        );
+
+        $input = new ArrayInput($arguments);
+        $returnCode = $command->run($input, $output);
+
+        $cmd = sprintf('rm -rf %s', escapeshellarg($tmpDir . '/*'));
+        $output->writeln($cmd);
+        exec($cmd);
+
+        $cmd = sprintf('cp -r %s %s', escapeshellarg($rootDir . '/Ressources/images'), escapeshellarg($tmpDir));
+        $output->writeln($cmd);
+        exec($cmd);
+
+        passthru(sprintf("cd %s && pdflatex output.latex", escapeshellarg($tmpDir)));
+        rename($tmpDir . "/output.pdf", $filename);
 
         $output->writeln(sprintf('Write %s', $filename));
-    }
-
-    public function getTweetIdsToExport()
-    {
-        return json_decode(file_get_contents(__DIR__ . '/../../../../data/tweets.json'));
-    }
-
-    public function prepareTweet($twittId)
-    {
-          $twitt = $this->client->get(sprintf('statuses/show/%s.json', $twittId))->send()->json();
-          $expandedUrls = array();
-          foreach ($twitt['entities']['urls'] as $urlInfos) {
-            $expandedUrls[] = $urlInfos['expanded_url'];
-          }
-          $export = array(
-            'tweet_id' => $twitt['id_str'],
-            'user' => array(
-              'screen_name' => $twitt['user']['screen_name'],
-            ),
-            'in_reply_to_status_id'      => $twitt['in_reply_to_status_id_str'],
-            'in_reply_to_user_id'        => $twitt['in_reply_to_user_id_str'],
-            'timestamp'                  => strtotime($twitt['created_at']), //TODO décalage de 1 sur la date problème fuseau horaire ?
-            'source'                     => $twitt['source'],
-            'text'                       => $twitt['text'],
-            'retweeted_status_id'        => '', //TODO
-            'retweeted_status_user_id'   => '', //TODO
-            'retweeted_status_timestamp' => '', //TODO
-            'expanded_urls'              => implode(',', $expandedUrls),
-          );
-          return $export;
     }
 }
